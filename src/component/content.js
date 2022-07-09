c136677113bb45dfcc0f177434a4d4c66a12e7c7
HieuLC <leconghieu1998@gmail.com>
2022-07-09 17:58:17 +0700

    load but not run yet

--- src/component/content.js
+++ src/component/content.js
@@ -1,10 +1,29 @@
 import React from "react";
-import Board from "./board"
+import {start} from "snake-wasm";
 
 class Content extends React.Component {
+	componentDidMount() {
+		this.loadWasm();
+	}
+
+	loadWasm = async () => {
+		try {
+			const wasm = await import("../../node_modules/snake-wasm/snake_wasm");
+			console.log("snake-wasm: ", wasm, start);
+			start();
+		//wasm
+		//	.default()
+		//	.then( () => {
+		//		wasm.start();
+		//	});
+		} catch(err) {
+			console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
+		}
+	};
+
 	render() {
 		return (
-			<Board />
+			<div key="canvas"></div>
 		);
 	}
 }
