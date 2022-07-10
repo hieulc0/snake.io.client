1df80c1ec536fcc75584fd660f24e3755c5086f6
HieuLC <leconghieu1998@gmail.com>
2022-07-10 14:38:33 +0700

    add dom element from rust-wasm to react

--- src/component/content.js
+++ src/component/content.js
@@ -1,28 +1,42 @@
 import React from "react";
-import * as snake_wasm from "snake-wasm";
+import * as snakeWasm from "snake-wasm";
 
 class Content extends React.Component {
+	constructor(props) {
+		super(props);
+		this.myRef = React.createRef();
+		this.state = {
+			canvas: document.createElement("div"),
+		}
+	}
+
 	componentDidMount() {
 		this.loadWasm();
 	}
 
 	loadWasm = async () => {
 		try {
-			//const wasm = await import("../../node_modules/snake-wasm/snake_wasm");
-			console.log("wasm: ", snake_wasm);
-			//start();
-			//greet("hello world, from react");
-			snake_wasm
+			console.log("wasm: ", snakeWasm);
+			snakeWasm
 				.default()
 				.then( () => {
-					snake_wasm.greet("hello, world! From react.");
+					snakeWasm.greet("hello, world! From react.");
+					let res = snakeWasm.start();
+					console.log("res of start() func: ", res);
+					this.setState({
+						canvas: res
+					});
+					console.log("current canvas: ", this.state.canvas);
+					this.myRef.current.appendChild(res);
 				});
 		} catch(err) {
 			console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
 		}
-	}; render() {
+	};
+
+	render() {
 		return (
-			<div key="canvas"></div>
+			<div key="canvas" ref={this.myRef}></div>
 		);
 	}
 }
